2f6d677688210ab502ca9ced015f417e1523323d
Hrvoje Tonkovac <hrvoje.tonkovac@toptal.com>
2022-11-19 23:14:55 +0100

    x

---
+++ index.ts
@@ -0,0 +1,7 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-native/resources";
+import * as storage from "@pulumi/azure-native/storage";
+
+// Create an Azure Resource Group
+const resourceGroup = new resources.ResourceGroup("kuberneti-konfeti");
+
